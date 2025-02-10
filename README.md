48c8d8823d1900a6f84e434da056cba3924342c6
Horokhov Aleksandr <aleksandr.gorokhov@rubygarage.org>
2025-02-10 10:37:25 +0200

    Update gem versions in template.rb for compatibility and improvements

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ```bash
-  rails _8.0.0_ new my_project -d postgresql -T -m template.rb
+  rails _8.0.1_ new my_project -d postgresql -T -m template.rb
 ```
--- template.rb
+++ template.rb
@@ -2,37 +2,42 @@
 
 require_relative 'helpers'
 
-create_file '.ruby-version', '3.3.6', force: true
+create_file '.ruby-version', '3.4.1', force: true
 
-gem 'haml-rails', '~> 2.1'
+gem 'draper', '~> 4.0.4'
 gem 'pundit', '~> 2.4'
-gem 'rails-i18n', '~> 8.0.0'
-gem 'tailwindcss-rails', '~> 3.0.0'
+gem 'rails-i18n', '~> 8.0.1'
+gem 'tailwindcss-rails', '~> 4.0.0'
+gem 'pagy', '~> 9.3.3'
 
 # Admin panel
 # gem 'activeadmin', '~> 3.2', '>= 3.2.5'
 # gem 'activeadmin_addons', '~> 1.10', '>= 1.10.2'
 
 # FrontEnd
+gem 'haml-rails', '~> 2.1'
 gem 'simple_form', '~> 5.3', '>= 5.3.1'
-gem 'simple_form-tailwind', '~> 0.1.1'
+gem 'simple_form-tailwind', '~> 0.1.2'
 
 gem_group :development, :test do
-  gem 'bullet', '~> 8.0.0'
+  gem 'bullet', '~> 8.0.1'
+  gem 'factory_bot_rails', '~> 6.4.4'
+  gem 'rspec-rails', '~> 7.1.1'
 end
 
 gem_group :development do
   gem 'bundle-audit', '~> 0.1.0', require: false
   gem 'fasterer', '~> 0.11.0', require: false
-  gem 'i18n-tasks', '~> 1.0', require: false
-  gem 'haml_lint', '~> 0.59.0', require: false
-  gem 'rubocop', '~> 1.69', require: false
-  gem 'rubocop-performance', '~> 1.23', require: false
-  gem 'rubocop-rspec', '~> 3.2', require: false
-  gem 'lefthook', '~> 1.9', require: false
-  gem 'database_consistency', '~> 1.7.26', require: false
-  gem 'strong_migrations', '~> 2.1'
-  gem 'annotaterb', '~> 4.13'
+  gem 'i18n-tasks', '~> 1.0.14', require: false
+  gem 'haml_lint', '~> 0.60.0', require: false
+  gem 'rails_best_practices', '~> 1.23.2', require: false
+  gem 'rubocop', '~> 1.71.2', require: false
+  gem 'rubocop-performance', '~> 1.23.1', require: false
+  gem 'rubocop-rspec', '~> 3.4', require: false
+  gem 'lefthook', '~> 1.10.10', require: false
+  gem 'database_consistency', '~> 2.0.4', require: false
+  gem 'strong_migrations', '~> 2.2.0'
+  gem 'annotaterb', '~> 4.13.0'
 end
 
 after_bundle do
